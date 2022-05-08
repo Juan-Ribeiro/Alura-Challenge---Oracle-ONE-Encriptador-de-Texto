e89b479a65d2c855b3f4622e38fbf1a054e176c9
Juan Ribeiro <ribeiro.jer@hotmail.com>
2022-05-07 21:19:29 -0300

    Mejorada implementación de funcion-encriptar.js y funcion-desencriptar.js * Se cambiaron ambas funcionas por una implementación más eficaz utilizando la función replace.all. * Se cambió el orden en el que trabajan las claves, ahora se utiliza el orden dado por la consigna para evitar posibles errores de codificación.

--- js/funcion-desencriptar.js
+++ js/funcion-desencriptar.js
@@ -4,41 +4,9 @@ botonDesencriptar.addEventListener("click", function () {
 });
 
 function desencriptarTexto(textoIngresado) {
-
-    let textoDesencriptado = "";
-
-    let letraActual;
-    let posicionLetraActual = 0;
-    while (posicionLetraActual < textoIngresado.length) {
-
-        letraActual = textoIngresado[posicionLetraActual];
-
-        switch (letraActual) {
-            case "a":
-                textoDesencriptado += "a";
-                posicionLetraActual += 2;
-                break
-            case "e":
-                textoDesencriptado += "e";
-                posicionLetraActual += 5;
-                break
-            case "i":
-                textoDesencriptado += "i";
-                posicionLetraActual += 4;
-                break
-            case "o":
-                textoDesencriptado += "o";
-                posicionLetraActual += 4;
-                break
-            case "u":
-                textoDesencriptado += "u";
-                posicionLetraActual += 4;
-                break
-            default:
-                textoDesencriptado += letraActual;
-                posicionLetraActual++;
-        }
-    }
-
-    return textoDesencriptado;
+    return textoIngresado.replaceAll("enter", "e")
+        .replaceAll("imes", "i")
+        .replaceAll("ai", "a")
+        .replaceAll("ober", "o")
+        .replaceAll("ufat", "u");
 }
--- js/funcion-encriptar.js
+++ js/funcion-encriptar.js
@@ -1,4 +1,3 @@
-
 botonEncriptar.addEventListener("click", function () {
 
     let textoIngresado = campoTextoIngresado.value;
@@ -7,30 +6,9 @@ botonEncriptar.addEventListener("click", function () {
 });
 
 function encriptarTexto(textoIngresado) {
-
-    let textoEncriptado = "";
-
-    for (let i = 0; i < textoIngresado.length; i++) {
-        textoEncriptado += encriptarCaracter(textoIngresado[i]);
-    }
-
-    return textoEncriptado;
-
-}
-
-function encriptarCaracter(caracter) {
-    switch (caracter) {
-        case "a":
-            return "ai";
-        case "e":
-            return "enter"
-        case "i":
-            return "imes";
-        case "o":
-            return "ober";
-        case "u":
-            return "ufat";
-        default:
-            return caracter;
-    }
+    return textoIngresado.replaceAll("e", "enter")
+        .replaceAll("i", "imes")
+        .replaceAll("a", "ai")
+        .replaceAll("o", "ober")
+        .replaceAll("u", "ufat");
 }
